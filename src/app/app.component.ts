d8919d4d60ec0665e65b8709098281f25e71ec4c
Heliya Rahbar <zahrarahbar@gmail.com>
2021-11-06 15:44:59 +0330

    find list of items

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { TestComponent } from 'src/app/test/test.component';
 
@@ -9,15 +9,28 @@ import { TestComponent } from 'src/app/test/test.component';
 })
 export class AppComponent {
   title = 'tour-test';
+  itemHeight: number = 0;
+  itemWidth: number = 0;
+  items: string[] = ['.item-1', '.item-2', '.item-3', '.item-4', '.item-5'];
+  curentItem: number = 0;
+
+  @ViewChild('item') item: any;
 
   constructor(public dialog: MatDialog) {}
 
-  ngOnInit() {
-    var div = document.querySelector('.item-5');
-    var divOffset = this.offset(div);
-    console.log(divOffset.left, divOffset.top);
+  ngOnInit() {}
 
-    this.dialog.open(TestComponent, {
+  ngAfterViewInit() {
+    this.findItem(this.curentItem);
+  }
+
+  findItem(currenvtItem: number) {
+    let div = document.querySelector(this.items[currenvtItem]);
+    let divOffset = this.offset(div);
+    this.itemWidth = this.item.nativeElement.offsetWidth;
+    this.itemHeight = this.item.nativeElement.offsetHeight;
+
+    let dialogRef = this.dialog.open(TestComponent, {
       width: '100%',
       height: '100%',
       maxWidth: '100vw',
@@ -25,13 +38,21 @@ export class AppComponent {
       panelClass: 'class-test',
       data: {
         position: { top: divOffset.top, left: divOffset.left },
+        item: { width: this.itemWidth, height: this.itemHeight },
         value: div?.innerHTML,
       },
+    });
+
+    dialogRef.afterClosed().subscribe((result) => {
+      if (result && result?.onNext == true) {
+        this.curentItem++;
+        this.findItem(this.curentItem);
+      }
     });
   }
 
   offset(el: any) {
-    var rect = el.getBoundingClientRect(),
+    let rect = el.getBoundingClientRect(),
       scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
       scrollTop = window.pageYOffset || document.documentElement.scrollTop;
     return {
--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -1,5 +1,11 @@
-import { Component, Inject, OnInit } from '@angular/core';
-import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+import {
+  Component,
+  ElementRef,
+  Inject,
+  OnInit,
+  ViewChild,
+} from '@angular/core';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-test',
@@ -9,45 +15,111 @@ import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 export class TestComponent implements OnInit {
   width = 0;
   height = 0;
+  @ViewChild('child') child: any;
 
-  constructor(@Inject(MAT_DIALOG_DATA) public data: any) {}
+  constructor(
+    @Inject(MAT_DIALOG_DATA) public data: any,
+    private ref: MatDialogRef<TestComponent>
+  ) {}
 
   ngOnInit(): void {
     this.width = window.innerWidth;
     this.height = window.innerHeight;
     this.draw();
-    console.log(this.data);
+  }
+
+  ngAfterViewInit() {
+    // right side of page
+    if (this.data.position.left > this.width / 2) {
+      console.log(this.data.position.left, this.width / 2, 'right');
+      if (this.data.position.top > this.height / 2) {
+        console.log(this.data.position.top, this.height / 2, 'bottom');
+        this.child.nativeElement.setAttribute(
+          'style',
+          `right:${
+            this.width - this.data.position.left - this.data.item.width
+          }px;max-width:${this.data.position.left + 50}px;bottom:${
+            this.data.position.top - this.data.item.height
+          }px`
+        );
+      } else {
+        console.log(this.data.position.top, this.height / 2, 'top');
+        this.child.nativeElement.setAttribute(
+          'style',
+          `right:${
+            this.width - this.data.position.left - this.data.item.width
+          }px;max-width:${this.data.position.left + 50}px;top:${
+            this.data.position.top + this.data.item.height
+          }px`
+        );
+      }
+    }
+
+    // left side of page
+    if (this.data.position.left <= this.width / 2) {
+      console.log(this.data.position.left, this.width / 2, 'left');
+      if (this.data.position.top > this.height / 2) {
+        console.log(this.data.position.top, this.height / 2, 'bottom');
+        this.child.nativeElement.setAttribute(
+          'style',
+          `left:${this.data.position.left}px;max-width:${
+            this.width - this.data.position.left - 50
+          }px;bottom:${this.data.position.top - this.data.item.height}px`
+        );
+      } else {
+        console.log(this.data.position.top, this.height / 2, 'top');
+        this.child.nativeElement.setAttribute(
+          'style',
+          `left:${this.data.position.left}px;max-width:${
+            this.width - this.data.position.left - 50
+          }px;top:${this.data.position.top + this.data.item.height}px`
+        );
+      }
+    }
   }
 
   draw() {
     let canvas = document.getElementById('canvas') as HTMLCanvasElement;
-    canvas.width = this.width;
-    canvas.height = this.height;
+    canvas.width = this.width - 6;
+    canvas.height = this.height - 6;
     let context: any = canvas.getContext('2d');
 
+    this.doBackDrop(context);
     this.doDraw(context);
   }
 
+  doBackDrop(context: CanvasRenderingContext2D) {
+    context.globalAlpha = 0.6;
+    context.fillStyle = '#A8A8A8';
+    const backgroundPath = new Path2D();
+    backgroundPath.moveTo(0, 0);
+    backgroundPath.lineTo(0, this.height);
+    backgroundPath.lineTo(this.width, this.height);
+    backgroundPath.lineTo(this.width, 0);
+    backgroundPath.closePath();
+    context.fill(backgroundPath);
+    context.globalAlpha = 1;
+  }
+
   doDraw(context: CanvasRenderingContext2D) {
-    context.globalAlpha = 0.3;
+    context.globalAlpha = 0.6;
+    context.fillStyle = '#01806b';
     const backgroundPath = new Path2D();
     let controlY = this.height - 100;
-    let controlXDelta = this.width / 4;
     backgroundPath.moveTo(0, 0);
     backgroundPath.lineTo(this.width, 0);
     backgroundPath.lineTo(this.width, this.height * 0.7);
-    backgroundPath.bezierCurveTo(
-      this.width - controlXDelta,
-      controlY,
-      controlXDelta,
+    backgroundPath.quadraticCurveTo(
+      this.width / 2,
       controlY,
       0,
       this.height * 0.7
     );
 
     backgroundPath.closePath();
+    backgroundPath.arc(100, 75, 50, 0, 2 * Math.PI);
+
     context.fill(backgroundPath);
-    context.fillRect(0, 0, window.innerWidth, window.innerHeight);
     context.globalAlpha = 1;
     context.clearRect(
       this.data.position.left,
@@ -55,5 +127,13 @@ export class TestComponent implements OnInit {
       100,
       100
     );
+  }
+
+  onCancle() {
+    this.ref.close();
+  }
+
+  onNext() {
+    this.ref.close({ onNext: true });
   }
 }
